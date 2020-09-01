dd111c491c8af968c37415fdb583dd5cabd6cd06
Merkwurdichliebe <tal.zana@gmail.com>
2020-09-01 15:44:28 +0200

    get_path renamed

--- epidemic.py
+++ epidemic.py
@@ -125,7 +125,7 @@ def initialize():
     return [draw, discard, exile, cardpool]
 
 
-def get_filename(filename):
+def get_path(filename):
     name = os.path.splitext(filename)[0]
     ext = os.path.splitext(filename)[1]
     if platform.system() == "Darwin":
@@ -140,7 +140,7 @@ def get_initial_deck():
     # Initialize the initial deck from the available cards list in cards.yml
     # file = os.path.realpath('data/cards.yml')
     # file = NSBundle.mainBundle().pathForResource_ofType_("data/cards", "yml")
-    file = get_filename('data/cards.yml')
+    file = get_path('data/cards.yml')
     init_deck = Deck('Starter Deck')
     valid_colors = ['blue', 'yellow', 'black', 'green']
 
