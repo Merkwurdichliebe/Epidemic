7d1c04d80e30c93b04b75ed36590b15bd760cf8b
Merkwurkdichliebe <tal.zana@gmail.com>
2020-09-22 12:44:26 +0200

    Refactored yaml read methods

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ import yaml
 
 class Game:
     def __init__(self):
-        self.games = self.read_decks_from_file()
+        self.games = self.get_all_games()
         self.deck = None
         self.stats = None
 
@@ -18,7 +18,7 @@ class Game:
         """Prepare the initial state for the game. Initialise all decks.
         This is run once at the start of every game."""
 
-        # Create a list with the Draw Deck and the 3 other empty decks
+        # Create a list with the Draw Deck and the 2 other empty decks
         game_decks = [self.initialise_draw_deck(game),
                       Deck('discard'),
                       Deck('exclude')]
@@ -31,8 +31,7 @@ class Game:
 
     def initialise_draw_deck(self, game):
         d = DrawDeck('draw')
-        # Get a copy of the deck from the games list
-        d.add(deepcopy(self.games[game]))
+        d.add(deepcopy(self.games[game]))  # games list shouldn't mutate
         return d
 
     @staticmethod
@@ -41,44 +40,30 @@ class Game:
             from_deck.move(card, to_deck)
 
     def epidemic(self, card):
-        """Do the epidemic shuffle phase: draw a card from the bottom
-        of the Draw Deck, discard it and shuffle the discard pile back
-        onto the top of the Draw Deck."""
+        """Draw a card from the bottom of the Draw Deck, discard it
+        and shuffle the discard pile back onto the top of the Draw Deck."""
         new_card = self.deck['draw'].get_card_by_name(card)
         self.deck['draw'].remove_from_bottom(new_card)
-
-        # Add the card to the discard pile
         self.deck['discard'].add(new_card)
 
         # Create new card pool
-        # We use copy in order to reset the discard pile
-        # without affecting the newly pooled cards
         new_cards = Deck('epidemic')
         for card in self.deck['discard'].cards.copy():
             new_cards.add(card)
-
         self.deck['draw'].add(new_cards)
 
         # Clear the discard pile
         self.deck['discard'].clear()
 
-    def read_decks_from_file(self):
+    def get_all_games(self):
         # Initialize the initial deck from the card list in cards.yml
         game = {}
-        file = utility.get_path('data/cards.yml')
-
-        try:
-            with open(file, encoding='utf-8') as f:
-                data = yaml.load(f, Loader=yaml.FullLoader)
-        except FileNotFoundError as e:
-            print(f'Missing or damaged cards.yml configuration file\n({e})')
-
+        data = self.read_data_file()
         try:
             for game_title in data.keys():
                 game[game_title] = self.create_deck(game_title, data)
         except ValueError:
-            # Raise the error again to stop execution
-            raise
+            raise  # Raise the error again to stop execution
 
         return game
 
@@ -94,3 +79,14 @@ class Game:
                 for i in range(item['count']):
                     deck.add(card)
         return deck
+
+    @staticmethod
+    def read_data_file():
+        file = utility.get_path('data/cards.yml')
+        try:
+            with open(file, encoding='utf-8') as f:
+                data = yaml.load(f, Loader=yaml.FullLoader)
+        except FileNotFoundError as e:
+            print(f'Missing or damaged cards.yml configuration file\n({e})')
+        else:
+            return data
